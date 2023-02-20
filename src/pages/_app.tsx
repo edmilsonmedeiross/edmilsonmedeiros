c9f122416453edf212e442fec9030ff527136874
edmilsonmedeiross <edmilsonmedeiros1990@gmail.com>
2023-02-20 15:56:16 -0300

    feat: adicionado estilo global

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css';
+import { Lato } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import '../styles/globals.sass';
+
+const lato = Lato({
+  weight: ['300', '400', '700', '900'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import styles from '../styles/component.module.css';
+
 export default function Home() {
   return (
     <>
