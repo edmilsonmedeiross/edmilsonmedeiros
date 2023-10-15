f6643c406cd0236c77a35e33613ae7ad7ff1824e
edmilsonmedeiross <edmilson_jb@hotmail.com>
2023-10-15 19:07:43 -0300

    feat/ -> cria header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Link from "next/link";
+
+interface NavLink {
+  title: string;
+  url: string;
+}
+
+const navLinks: NavLink[] = [
+  { title: "Home", url: "/" },
+  { title: "About", url: "/about" },
+  { title: "Services", url: "/services" },
+  { title: "Contact", url: "/contact" },
+];
+
+const Header: React.FC = () => {
+  return (
+    <nav>
+      {navLinks.map((navLink) => (
+        <Link key={navLink.url} href={navLink.url}>
+          {navLink.title}
+        </Link>
+      ))}
+    </nav>
+  );
+};
+
+export default Header;
