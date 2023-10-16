97e266fb93a7032fa0d726391a26b51556b83531
edmilsonmedeiross <edmilson_jb@hotmail.com>
2023-10-16 00:49:05 -0300

    feat/ -> cria stacks

---
+++ src/utils/stacks.ts
@@ -0,0 +1,68 @@
+import docker from '../../public/image/docker.png'
+import git from '../../public/image/git.jpeg'
+import html from '../../public/image/html.png'
+import next from '../../public/image/next.png'
+import node from '../../public/image/node.png'
+import react from '../../public/image/react.jpeg'
+import redux from '../../public/image/redux.jpeg'
+import styled from '../../public/image/styled.png'
+import tailwind from '../../public/image/tailwind.jpeg'
+import typescript from '../../public/image/typescript.png'
+import vscode from '../../public/image/vscode.jpeg'
+import prisma from '../../public/image/prisma.png'
+import mysql from '../../public/image/mysql.png'
+
+export const stacks = [
+  {
+    name: 'Docker',
+    image: docker,
+  },
+  {
+    name: 'Git',
+    image: git,
+  },
+  {
+    name: 'HTML',
+    image: html,
+  },
+  {
+    name: 'Next',
+    image: next,
+  },
+  {
+    name: 'Node',
+    image: node,
+  },
+  {
+    name: 'React',
+    image: react,
+  },
+  {
+    name: 'Redux',
+    image: redux,
+  },
+  {
+    name: 'Styled',
+    image: styled,
+  },
+  {
+    name: 'Tailwind',
+    image: tailwind,
+  },
+  {
+    name: 'Typescript',
+    image: typescript,
+  },
+  {
+    name: 'VSCode',
+    image: vscode,
+  },
+  {
+    name: 'Prisma',
+    image: prisma,
+  },
+  {
+    name: 'MySQL',
+    image: mysql,
+  },
+]
